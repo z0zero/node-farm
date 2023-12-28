11dd020df2e3b24b2c7ecbeebf219cd941fdeab0
z0zero <briansangapta@gmail.com>
2023-12-28 09:16:01 +0700

    Simple API

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const http = require('http');
 const url = require('url');
 
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
@@ -12,6 +16,9 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
